b478ce1ed24e1066df1c387efa8d36380a8f81d3
Rizky Benggolo <travarilo@gmail.com>
2018-12-24 09:44:33 +0700

    Revert "cpufreq: add some governors"
    This reverts commit 485caa28333a101bd20d163a4505a7734348d233.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -537,15 +537,6 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
-extern struct cpufreq_governor cpufreq_gov_nightmare;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
-extern struct cpufreq_governor cpufreq_gov_darkness;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSV5)
-extern struct cpufreq_governor cpufreq_gov_darknessv5;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknessv5)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
