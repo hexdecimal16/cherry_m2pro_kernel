2dadcb87a0558d03b75d3829dbb8e43e006f4c0f
Rizky Benggolo <travarilo@gmail.com>
2018-12-24 09:44:33 +0700

    cpufreq: add some governors
    alucard, darkness, darknessv5, nightmare
    
    Change-Id: I0f9a61ea8c97a93a5167c8abb66d4012d821c0c8
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	drivers/cpufreq/cpufreq_alucard.c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -540,6 +540,18 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSV5)
+extern struct cpufreq_governor cpufreq_gov_darknessv5;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknessv5)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
