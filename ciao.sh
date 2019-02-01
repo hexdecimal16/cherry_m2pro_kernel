55d1509b905ce5b022f5f42145fe31f5ac0e0a78
derflacco <paul89rulez@gmail.com>
2019-02-01 19:27:19 +0100

    preparing sources for cherry-pick

---
+++ ciao.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+echo "--------------------------------------------"
+echo "Welcome To Kernel Source"
+echo "Lets Compile kernel sit Back Have A coffee "
+echo "--------------------------------------------"
+
+chmod a+x qcacld-update.sh
+./qcacld-update.sh 
+rm -r output
+export ARCH=arm64
+export CROSS_COMPILE=/home/derflacco/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+clear
+make clean
+make mrproper
+mkdir output
+make -C $(pwd) O=output drg_defconfig
+make -j32 -C $(pwd) O=output
+cp -r output/arch/arm64/boot/Image.gz-dtb /home/derflacco/BOOTIMAGES
+
+echo "--------------------------------------------"
+echo "Done"
+echo "--------------------------------------------"
