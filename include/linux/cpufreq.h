5c7d4233f65850fe8ff1433b3c75acbc0fea6d5e
Hexdecimal16 <Hexdecimal16@gmail.com>
2019-03-26 09:59:13 -0400

    Revert "Governor: added intelliactive and intellidemand"
    This reverts commit 5b466624382c75170ebce2fc9e6bb0c65e27c817.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -546,12 +546,6 @@ extern struct cpufreq_governor cpufreq_gov_cultivation;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TRIPNDROID)
 extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_tripndroid)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
-extern struct cpufreq_governor cpufreq_gov_intellidemand;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
-extern struct cpufreq_governor cpufreq_gov_intelliactive;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
