ArrowOS-Devices/android_kernel_asus_X01BD

sdm660: Adapt Anykernel script for m2pro_defconfig

script.sh
@@ -8,11 +8,11 @@ DATE_POSTFIX=$(date +"%Y%m%d")
 KERNEL_DIR=$PWD
 KERNEL_TOOLCHAIN=$PWD/../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 CLANG_TOOLCHAIN=$PWD/../clang/bin/clang-9
-KERNEL_DEFCONFIG=sanders_defconfig
+KERNEL_DEFCONFIG=m2pro_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS=16
 ZIP_DIR=$KERNEL_DIR/zip/
-FINAL_KERNEL_ZIP=MAYHEM-KERNEL~beast-mod-Release-$DATE_POSTFIX.zip
+FINAL_KERNEL_ZIP=ETHERIOUS-KERNEL~FireDragonKing-mod-Release-$DATE_POSTFIX.zip
 # Speed up build process
 MAKE="./makeparallel"
 
@@ -37,7 +37,7 @@ make clean && make mrproper && rm -rf out/
 
 echo -e "$cyan // defconfig is set to $KERNEL_DEFCONFIG //"
 echo -e "$blue***********************************************"
-echo -e "$R          BUILDING MAYHEMKERNEL          "
+echo -e "$R          BUILDING ETHERIOUSKERNEL          "
 echo -e "***********************************************$nocol"
 make $KERNEL_DEFCONFIG O=out
 make -j$JOBS CC=$CLANG_TOOLCHAIN CLANG_TRIPLE=aarch64-linux-android- O=out

zip/anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=// MAYHEM-KERNEL by @Ronax //
+kernel.string=// ETHERIOUS-KERNEL by Sonal Singh(@Sksbss) //
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=sanders
-device.name2=Moto G5S Plus
-device.name3=sanders_retail
+device.name1=X01BD
+device.name2=Asus Zenfone Max Pro M2
+device.name3=
 device.name4=
 device.name5=
 supported.versions=

hexdecimal16/cherry_m2pro_kernel

script.sh
@@ -8,11 +8,11 @@ DATE_POSTFIX=$(date +"%Y%m%d")
 KERNEL_DIR=$PWD
 KERNEL_TOOLCHAIN=$PWD/../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 CLANG_TOOLCHAIN=$PWD/../clang/bin/clang-9
-KERNEL_DEFCONFIG=sanders_defconfig
+KERNEL_DEFCONFIG=m2pro_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS=16
 ZIP_DIR=$KERNEL_DIR/zip/
-FINAL_KERNEL_ZIP=MAYHEM-KERNEL~beast-mod-Release-$DATE_POSTFIX.zip
+FINAL_KERNEL_ZIP=CHERRY-KERNEL~finelypickedcherries-mod-Release-$DATE_POSTFIX.zip
 # Speed up build process
 MAKE="./makeparallel"
 
@@ -37,7 +37,7 @@ make clean && make mrproper && rm -rf out/
 
 echo -e "$cyan // defconfig is set to $KERNEL_DEFCONFIG //"
 echo -e "$blue***********************************************"
-echo -e "$R          BUILDING MAYHEMKERNEL          "
+echo -e "$R          BUILDING CHERRYKERNEL          "
 echo -e "***********************************************$nocol"
 make $KERNEL_DEFCONFIG O=out
 make -j$JOBS CC=$CLANG_TOOLCHAIN CLANG_TRIPLE=aarch64-linux-android- O=out

zip/anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=// MAYHEM-KERNEL by @Ronax //
+kernel.string=// CHERRY-KERNEL by DHAIRY TRIPATHI(@Dhairy) //
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=sanders
-device.name2=Moto G5S Plus
-device.name3=sanders_retail
+device.name1=X01BD
+device.name2=Asus Zenfone Max Pro M2
+device.name3=
 device.name4=
 device.name5=
 supported.versions=