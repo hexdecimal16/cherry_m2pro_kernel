3a6ad7a788b7d0b22f0dc5755db09e9b666dc408
Hexdecimal16 <Hexdecimal16@gmail.com>
2019-06-03 00:42:00 +0530

    kernel:fork.c: add and remove includes
    - removed unnecessary includes of cpu_input_boost abd devfreq_boost
    - add required inlude of cpufreq_times

--- kernel/fork.c
+++ kernel/fork.c
@@ -79,8 +79,7 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
+#include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
